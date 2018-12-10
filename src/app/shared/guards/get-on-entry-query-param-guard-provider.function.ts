89865f6c41544d114d7de7e4c26b9a68ffd5423c
John Heinnickel` <jheinnic@gmail.com>
2018-12-09 20:44:42 -0800

    Guards to work around Angular's removal of query parmas on application entry points

---
+++ src/app/shared/guards/get-on-entry-query-param-guard-provider.function.ts
@@ -0,0 +1,32 @@
+import {FactoryProvider, inject, InjectionToken, Provider} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+
+export function getOnEntryQueryParamGuardProvider(
+  injectionToken: InjectionToken<CanActivate>, paramName: string): FactoryProvider {
+  return {
+    provide: injectionToken,
+    useFactory: (router: Router) => {
+      new OnEntryQueryParamGuard(router, paramName)
+    },
+    deps: [Router]
+  };
+}
+
+class OnEntryQueryParamGuard implements CanActivate {
+  constructor(private readonly router: Router, private readonly paramName: string) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean
+  {
+    if (! route.queryParamMap.has(this.paramName)) {
+      if ((Object.keys(route.queryParams).length > 0) !== window.location.href.includes('?')) {
+        const url = new URL(window.location.href);
+        this.router.navigateByUrl(url.pathname + url.search);
+      }
+
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ src/app/shared/guards/get-query-param-guard-provider.function.ts
@@ -0,0 +1,22 @@
+import {FactoryProvider, inject, InjectionToken, Provider} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+
+export function getQueryParamGuardProvider(
+  injectionToken: InjectionToken<CanActivate>, paramName: string): FactoryProvider {
+  return {
+    provide: injectionToken,
+    useFactory: () => {
+      new QueryParamGuard(paramName)
+    },
+  };
+}
+
+class QueryParamGuard implements CanActivate {
+  constructor(private readonly paramName: string) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean
+  {
+    return route.queryParamMap.has(this.paramName);
+  }
+}
