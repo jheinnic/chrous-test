ca528428ebbb3035d700058ea87bd9ee2b37fde8
John Heinnickel` <jheinnic@gmail.com>
2018-12-09 20:44:59 -0800

    Completing name change from chorustest to chorus-test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to chorustest!');
+    expect(page.getTitleText()).toEqual('Welcome to chorus-test!');
   });
 });
